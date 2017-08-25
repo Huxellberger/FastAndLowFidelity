b9f9a18ba1f1389682b021a6136956072c6d448b
Jake Huxell <darkduckmovies@gmail.com>
2017-08-25 16:15:42 +0100

    First Pass Of Drawing Road

--- Source/Toretto.asm
+++ Source/Toretto.asm
@@ -15,14 +15,30 @@
 				; Start Code here (Start of the ROM memory space)
 				seg Code
 				org $F000
-
+				
 ; ------------------------------------------------------------------
 ;				Variables In RAM
 ; ------------------------------------------------------------------
 
-BGCOLOUR		= #$18 ; Dusty Desert
+ROADTOP			= $80 ; Top of road
+ROADBOT			= $81 ; Bottom of road
+ROADCENTRE		= $82 ; Centre line of road
+ROADCALC		= $83 ; Have we calculated the road?
+ROADWIDTHHALF	= $84 ; Half of RoadWidth
+DRAWINGPF		= $85 ; Currently Drawing the playfield
+CURRENTROAD		= $86 ; Shadow of what a is holding when drawing the road
+
+; ------------------------------------------------------------------
+;				Variables In ROM
+; ------------------------------------------------------------------
+
+BGOFFCOLOUR		= #$18 ; Dusty Desert
+BGONCOLOUR		= #$06 ; Dark Grey
 FGCOLOUR		= #$0E ; White
 ROADWIDTH		= #$80 ; Width of road
+SCREENHEIGHT	= #192 ; Screen Height (Should be changed on PAL)
+ROADPATTERN		= #%00110011 ; Markings on the road default pattern
+ROADBOUNDRY		= #%11111111 ; Road Boundry
 
 ; ------------------------------------------------------------------
 ;				System Reset
@@ -45,16 +61,99 @@ NextFrame
 
 				; 192 visible scanlines make up our frame on NTSC
 				; Set Background Colour
-				lda BGCOLOUR
+				lda #BGOFFCOLOUR
 				sta COLUBK
+				lda #FGCOLOUR
+				sta COLUPF
 				
 				; Draw Playfield begin
-				ldx #192
+				ldx #SCREENHEIGHT
+				
+				; Only Calculate road boundries if first time
+				cmp #ROADCALC, #0
+				bne PrepareLV
+				
+				; Calcuate where the boundries to the road should be drawn
+				txa
+				lsr
+				sta ROADCENTRE
+				tay ; Can be used later! 
+				
+				; Work out max / 2
+				lda #ROADWIDTH
+				lsr
+				sta ROADWIDTHHALF
+				
+				; Now we know outside edge will be midpoint + (max / 2)
+				tya
+				adc #ROADWIDTHHALF
+				sta ROADTOP
+				
+				tya
+				sbc #ROADWIDTHHALF
+				sta ROADBOT
+				
+				; Make sure we never do that again
+				lda #1
+				sta ROADCALC
+				
+				; load Top Road boundry into accumulator (First to be drawn)
+PrepareLV		
+				ldy #0 ; Using to say if we should draw the midpoint of the road
 LVScan
 				sta WSYNC
+				
+				cmp #DRAWINGPF,#1
+				bne DrawLogic
+				lda #0
+				sta PF0
+				sta PF1
+				sta PF2
+				sta DRAWINGPF
+DrawLogic
+				cmp	#CURRENTROAD,x
+				bne NotRoad
+				
+				; Draw the piece of the road
+				cpy #0
+				bne CentreDraw 
+				
+				lda #ROADBOUNDRY
+				sta PF0
+				sta PF1
+				sta PF2
+				
+				; if not the centre we change BG colour
+				cmp #COLUBK,#BGOFFCOLOUR
+				beq DrawOnRoad
+				
+				; Set to off road colour
+				lda #BGOFFCOLOUR
+				sta COLUBK
+				jmp GetRoadCentre
+DrawOnRoad
+				; set to on-road colour
+				lda #BGONCOLOUR
+				sta COLUBK
+				
+GetRoadCentre
+				
+				lda #ROADCENTRE
+				sta CURRENTROAD
+				ldy #1 ; Now we're drawing the centre
+				jmp NotRoad
+CentreDraw		
+
+				lda #ROADPATTERN
+				sta PF0
+				sta PF1
+				sta PF2
+				ldy #0 ; No Longer drawing the centre
+				lda #ROADBOT
+				sta CURRENTROAD
+NotRoad
 				dex
 				bne LVScan
-				
 				
 ; ------------------------------------------------------------------
 ;				Handle Overscan (NTSC)
